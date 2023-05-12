e8fe98155e8e97b6783eb11223b71f3f61993d16
easyhyun00 <leejihyun0324@gmail.com>
2023-05-12 09:56:51 +0900

    필요없는 파일 삭제, 파이어베이스 키 숨기기

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth"
-import { getStorage } from "firebase/storage"
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAcAFY0fLbjAYJEHABHxrjb8-IRTYOuHL4",
-    authDomain: "em25-c1a70.firebaseapp.com",
-    databaseURL: "https://em25-c1a70-default-rtdb.firebaseio.com",
-    projectId: "em25-c1a70",
-    storageBucket: "em25-c1a70.appspot.com",
-    messagingSenderId: "588520894878",
-    appId: "1:588520894878:web:92fc7c999c151c7ceaa68f",
-    measurementId: "G-ZV8Y51RPQP"
-};
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import firebaseConfig from './firebaseConfigInfo';
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
